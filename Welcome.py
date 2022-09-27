ba2b1597c703a691fbde9b1beabd995d53affe16
RITAM CHARAN <ritamcharan232001@gmail.com>
2022-09-27 09:09:07 +0530

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,10 @@
 # add python codes to this repo
+import array as arr
 
 
+# print("Hello world ")
 
-print("Hello world ")
+My_Array=arr.array('i',[1,2,3,6])
+My_list=[1,'ac',1.20]
+print(My_Array)
+print(My_list)
